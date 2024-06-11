165b21cb33e2c89b388ee95a271856ca7d7a4d8f
Herbert Tamayo <hftamayo@gmail.com>
2024-06-11 13:15:26 -0600

    adding todo routes

---
+++ api/routes/todo.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hftamayo/gographqltodo/api/v1/todo"
+)
+
+func SetupRoutes(app *gin.Engine, handler *todo.Handler) {
+	const todoIDPath = "/gotodo/todo/:id"
+
+	app.POST("/gotodo/todo", handler.CreateTodo)
+
+	app.PATCH(todoIDPath+"/done", handler.UpdateTodoDone)
+
+	app.PATCH(todoIDPath, handler.UpdateTodo)
+
+	app.GET("/gotodo/todos", handler.GetAllTodos)
+
+	app.GET(todoIDPath, handler.GetTodoById)
+
+	app.DELETE(todoIDPath, handler.DeleteTodoById)
+
+}
